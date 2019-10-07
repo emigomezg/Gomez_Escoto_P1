8466a48a6ddf1e8f3abab7aae3c29b2ba21f683a
emigomezg <47527289+emigomezg@users.noreply.github.com>
2019-10-07 15:16:02 -0500

    Added doxygen style comments

--- MOTOR.h
+++ MOTOR.h
@@ -22,9 +22,38 @@ typedef struct{
 	uint8_t pin;
 }motor_pins_t;
 
-
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*!
+ \brief	 This function initialize the GPIO used for the motor and the motor LEDs. 
+ \param[in] struct cointaning the the bit as well as the port thats going to be used. 
+ \return void
+ \todo 
+ */
 void MOTOR_begin(led_motor_pins_t led1,led_motor_pins_t led2,motor_pins_t motor);
+
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*!
+ \brief	 This function turns the motor flag on and initialize the PIT for the desired turned on time.
+ \param[in] void
+ \return void
+ \todo 
+ */
 void MOTOR_on(void);
+
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*!
+ \brief	  This function turns the motor flag off, turns the motor off and also the LEDs. PIT timer 
+ 	  is also disable.
+ \param[in] struct cointaning the the bit as well as the port thats going to be used. 
+ \return void
+ \todo 
+ */
 void MOTOR_off(void);
 
 #endif /* MOTOR_H_ */
